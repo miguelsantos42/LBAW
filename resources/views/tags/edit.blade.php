72fb1d3650ba3c44c1fe88ec0183b77ec275170e
up201904710 <up201904710@fe.up.pt>
2023-12-12 02:09:52 +0000

    adde edit tags , Tags CRUD done

---
+++ resources/views/tags/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Edit Tag</h1>
+
+        <form action="{{ route('tags.update', $tag->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="tagName">Tag Name</label>
+                <input type="text" class="form-control" name="tagName" value="{{ old('tagName', $tag->tagName) }}" placeholder="Edit tag name">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Update</button>
+        </form>
+    </div>
+@endsection
+<
