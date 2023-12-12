30a62ef4a49d5d210ce509952fbc71e4d8eb1bca
Joao Francisco da Rocha Sequeira Alves <up202006281@fe.up.pt>
2023-12-12 11:59:41 +0000

    Upvote notification logic added

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\DB;
 
 use App\Models\Comment;
 use App\Models\Question;
+
+use App\Models\VoteNotification;
+use App\Models\Notification;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\VoteComment;
@@ -33,42 +37,70 @@ class CommentController extends Controller
 
   
     public function upvoteComment(Request $request, $commentid) {
-    $usersid = Auth::id();
-    $comment = Comment::findOrFail($commentid);
-    $existingVote = DB::table('votecomments')
-                      ->where('usersid', $usersid)
-                      ->where('commentid', $commentid)
-                      ->first();
-
-    if ($existingVote) {
-        if ($existingVote->updown === true) {
-            // Already upvoted, so remove the vote
-            $comment->votecount -= 1;
-            DB::table('votecomments')
-              ->where('usersid', $usersid)
-              ->where('commentid', $commentid)
-              ->delete();
+        $usersid = Auth::id();
+        $comment = Comment::findOrFail($commentid);
+        $existingVote = DB::table('votecomments')
+                          ->where('usersid', $usersid)
+                          ->where('commentid', $commentid)
+                          ->first();
+    
+    
+        if ($existingVote) {
+            if ($existingVote->updown === true) {
+                // Already upvoted, so remove the vote
+                $comment->votecount -= 1;
+                
+                DB::table('votecomments')
+                  ->where('usersid', $usersid)
+                  ->where('commentid', $commentid)
+                  ->delete();
+            } else {
+                // Change downvote to upvote
+                $comment->votecount += 2;
+                DB::table('votecomments')
+                  ->where('usersid', $usersid)
+                  ->where('commentid', $commentid)
+                  ->update(['updown' => true]);
+    
+                // Create a voteNotification record
+                $voteNotification = new VoteNotification([
+                    'updown' => TRUE,
+                    'usersId' => $comment->usersid,
+                    'commentId' => $commentid,
+                    'questionId' => NULL,
+                    'voterId' => $usersid,
+                ]);
+        
+                // Save the voteNotification
+                $voteNotification->save();
+            }
         } else {
-            // Change downvote to upvote
-            $comment->votecount += 2;
-            DB::table('votecomments')
-              ->where('usersid', $usersid)
-              ->where('commentid', $commentid)
-              ->update(['updown' => true]);
+            // First-time vote, add upvote
+            $comment->votecount += 1;
+            DB::table('votecomments')->insert([
+                'updown' => true, 
+                'usersid' => $usersid, 
+                'commentid' => $commentid
+            ]);
+    
+            // Create a voteNotification record
+            $voteNotification = new VoteNotification([
+                'updown' => TRUE,
+                'usersId' => $comment->usersid,
+                'commentId' => $commentid,
+                'questionId' => NULL,
+                'voterId' => $usersid,
+            ]);
+    
+            // Save the voteNotification
+            $voteNotification->save();
         }
-    } else {
-        // First-time vote, add upvote
-        $comment->votecount += 1;
-        DB::table('votecomments')->insert([
-            'updown' => true, 
-            'usersid' => $usersid, 
-            'commentid' => $commentid
-        ]);
+    
+        // Save the comment
+        $comment->save();
+    
+        return response()->json(['votecount' => $comment->votecount]);
     }
-
-    $comment->save();
-    return response()->json(['votecount' => $comment->votecount]);
-}
 
 public function downvoteComment(Request $request, $commentid) {
     $usersid = Auth::id();
